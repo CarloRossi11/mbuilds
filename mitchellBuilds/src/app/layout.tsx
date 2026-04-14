4b9afceb8c312314b21f0574943be65988be91c0
Berardelli <carlo.berardelli@sap.com>
2026-04-14 10:45:56 -0400

    updated favicon file location and og image url

--- mitchellBuilds/src/app/layout.tsx
+++ mitchellBuilds/src/app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
     siteName: "Mitchell Builds",
     images: [
       {
-        url: "https://techwrench.co/og-image.png",
+        url: "https://mitchellbuilds.netlify.app/og-image.png",
         width: 1200,
         height: 630,
       },
